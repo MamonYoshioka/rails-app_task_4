ec29d5d49b7a732d591647deb6d11f4124df77af
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-09-05 09:25:34 +0000

    [修正&add]userinfoのフォローボタン

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,24 +1,23 @@
 class RelationshipsController < ApplicationController
+  # followするとき
   def create
-    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-
-    #通知機能
-    @user.create_notification_follow!(current_user)
     redirect_to request.referer
   end
 
+  # followを外すとき
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
-  # フォローフォロワー一覧処理
+  # フォロー一覧
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
   end
 
+  # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
     @users = user.followers
